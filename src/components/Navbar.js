2bcc94ff4cfd780c36bcea04a3551df3352e09f6
Aakash Sharma <sharmaaakash824@gmail.com>
2022-06-07 16:01:59 +0530

    dark mode added on navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,8 +1,7 @@
 import React, { useState } from 'react';
-import { Link, useLocation, useNavigate } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
 
 const Navbar = () => {
-    let location = useLocation();
     let navigate = useNavigate();
     const [mode, setMode] = useState("light");
 
@@ -20,20 +19,20 @@ const Navbar = () => {
     return (
         <nav className={`navbar navbar-expand-lg bg-${mode}`}>
             <div className="container-fluid">
-                <Link className="navbar-brand" to="/">yourDiary</Link>
+                <Link className={`navbar-brand text-${mode === "dark" ? "light" : "dark"}`} to="/">yourDiary</Link>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
                     <ul className="navbar-nav me-auto mb-2 mb-lg-0">
                         <li className="nav-item">
-                            <Link className={`nav-link ${location.pathname === "/" ? "active" : ""}`} aria-current="page" to="/">Home</Link>
+                            <Link className={`nav-link text-${mode === "dark" ? "light" : "dark"}`} aria-current="page" to="/">Home</Link>
                         </li>
                         <li className="nav-item">
-                            <Link className={`nav-link ${location.pathname === "/about" ? "active" : ""}`} to="about">About</Link>
+                            <Link className={`nav-link text-${mode === "dark" ? "light" : "dark"}`} to="about">About</Link>
                         </li>
                     </ul>
-                    <div className="form-check form-switch mx-2">
+                    <div className={`form-check form-switch mx-2 text-${mode === "dark" ? "light" : "dark"}`}>
                         <input className="form-check-input" type="checkbox" role="switch" id="darkMode" onChange={onChange}/>
                             <label className="form-check-label" htmlFor="darkMode">{mode === "dark" ? "Light Mode" : "Dark Mode"}</label>
                     </div>
