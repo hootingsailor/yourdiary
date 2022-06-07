24bb755d9ca3a34d60092762e1800867cdfa202b
Aakash Sharma <sharmaaakash824@gmail.com>
2022-06-07 11:00:25 +0530

    about page added

--- src/components/About.js
+++ src/components/About.js
@@ -2,8 +2,43 @@ import React from 'react';
 
 const About = () => {
   return (
-    <div>
-      This is About
+    <div className="accordion" id="accordionExample">
+      <div className="accordion-item">
+        <h2 className="accordion-header" id="headingOne">
+          <button className="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+            yourDiary
+          </button>
+        </h2>
+        <div id="collapseOne" className="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+          <div className="accordion-body">
+            <strong>yourDiary </strong> is a Digital Diary where a user can store notes or basically write diary digitally.
+          </div>
+        </div>
+      </div>
+      <div className="accordion-item">
+        <h2 className="accordion-header" id="headingTwo">
+          <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+            About Me
+          </button>
+        </h2>
+        <div id="collapseTwo" className="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+          <div className="accordion-body">
+            I am a Full Stack MERN Developer and this is one of my project. My interest is designing Responsive UI for the user.   
+          </div>
+        </div>
+      </div>
+      <div className="accordion-item">
+        <h2 className="accordion-header" id="headingThree">
+          <button className="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+            Contact Us
+          </button>
+        </h2>
+        <div id="collapseThree" className="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+          <div className="accordion-body">
+            You can connect with me on Linkedin and see my work on github. My username is hootingsailor.
+          </div>
+        </div>
+      </div>
     </div>
   )
 }
